edb855b58a94c84cb378a5c2c800f9338d8383bc
Krrish Jaat <krrishjaat4763@outlook.com>
2026-03-13 13:15:06 +0530

    Update bootlogo-warning-fix.sh

--- recore/mods/bootlogo-warning-fix/bootlogo-warning-fix.sh
+++ recore/mods/bootlogo-warning-fix/bootlogo-warning-fix.sh
@@ -1,32 +1,17 @@
-#!/bin/bash
+# ==============================================================================
+#
+# MOD_NAME="Bootloader Warning Fix"
+# MOD_AUTHOR="Krrish"
+# MOD_DESC="Replace up_param.bin to remove unlocked bootloader warning."
+#
+# ==============================================================================
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-# Paths
-PARAM_DIR="$SCRIPT_DIR/../blobs/param"
-DEST_DIR="$OUT"
-
-# fallback if OUT not defined
-if [ -z "$DEST_DIR" ]; then
-    DEST_DIR="$SCRIPT_DIR/out"
-fi
-
-mkdir -p "$DEST_DIR"
-
-# Decide which file to copy
 if [ "$DEVICE_SINGLE_SYSTEM_IMAGE" = "essi" ]; then
-    SRC_FILE="$PARAM_DIR/up_param_1080p.bin"
-elif [ "$DEVICE_SINGLE_SYSTEM_IMAGE" = "essi64" ]; then
-    SRC_FILE="$PARAM_DIR/up_param_1440p.bin"
-else
-    SRC_FILE="$PARAM_DIR/up_param_1080p.bin"
-fi
+    ADD_FROM_FW "param" "param" "up_param_1080p.bin"
 
-# Check file
-if [ -f "$SRC_FILE" ]; then
-    echo "Copying $(basename "$SRC_FILE") to $DEST_DIR"
-    cp -f "$SRC_FILE" "$DEST_DIR/"
+elif [ "$DEVICE_SINGLE_SYSTEM_IMAGE" = "essi64" ]; then
+    ADD_FROM_FW "param" "param" "up_param_1440p.bin"
+
 else
-    echo "Error: $SRC_FILE not found!"
-    exit 1
+    ADD_FROM_FW "param" "param" "up_param_1080p.bin"
 fi
