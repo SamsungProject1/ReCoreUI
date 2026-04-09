e76e63c0db4b21586c032068a68e8b26a7e7b528
SamsungProject1 <keremkaantokat32@gmail.com>
2026-04-09 18:44:01 +0300

    Add m30s.sh with device configuration details

---
+++ objectives/m30s/m30s.sh
@@ -0,0 +1,67 @@
+
+CODENAME="m30s"
+PLATFORM="ex_9611"
+
+# Stock firmware details for download
+STOCK_MODEL="SM-M307F"
+STOCK_CSC="INS"
+STOCK_IMEI="351521110111119"
+
+# Source firmware details for download
+MODEL="SM-S711B"
+CSC="INS"
+IMEI="350411870309841"
+
+# Extra firmware (Optional) details for download
+EXTRA_MODEL=""
+EXTRA_CSC=""
+EXTRA_IMEI=""
+
+# Output
+FILESYSTEM="erofs"
+
+
+#
+# The following values are auto genarated by auto_gen.sh.
+# Manually uncomment and set values here to override default stock values.
+#
+
+###
+DEVICE_MODEL_NAME="Galaxy M30S"                          # Brand name from settings config
+DEVICE_SIOP_POLICY_FILENAME="siop_a51_exynos9611"                # Thermal/SIOP policy filename
+DEVICE_USE_STOCK_BASE="false"
+RECORE_CODENAME="shineui_m30s"
+
+### Display
+DEVICE_DISPLAY_HFR_MODE="2"                   # High Frame Rate Mode (0=60Hz)
+DEVICE_HAVE_HIGH_REFRESH_RATE="true"              # Device have high refresh rate or not
+DEVICE_DISPLAY_REFRESH_RATE_VALUES_HZ="60"      # Supported rates by display (e.g., 60,120)
+DEVICE_DEFAULT_REFRESH_RATE="60"                # Initial boot refresh rate
+DEVICE_HAVE_QHD_PANEL="false"                      # True if QHD display device
+DEVICE_HAVE_AMOLED_DISPLAY="false"                 # True if amoled display device
+DEVICE_AUTO_BRIGHTNESS_LEVEL="2"               # Light sensor behavior
+DEVICE_MDNIE_MODE="0"                          # Samsung mDnie color profile
+
+###
+SOURCE_HAVE_ESIM_SUPPORT="true"
+DEVICE_HAVE_ESIM_SUPPORT="false"
+
+### Audio
+DEVICE_HAVE_DUAL_SPEAKER="false"                   # true for Stereo, false for Mono
+
+### Extra features
+DEVICE_HAVE_SPEN_SUPPORT="false"                   # Device have SPen support
+DEVICE_HAVE_ESIM_SUPPORT="false"                   # Device have esim support
+DEVICE_HAVE_NPU="false"                            # Device have NPU
+
+### Build Properties
+DEVICE_FIRST_API_VERSION="9"                   # ro.vendor.build.version.release
+DEVICE_FIRST_SDK_VERSION="28"                   # ro.vendor.build.version.sdk
+DEVICE_VNDK_VERSION="33"                      # VNDK version
+DEVICE_SINGLE_SYSTEM_IMAGE="essi"                 # ro.product.system.device
+
+### External
+DEVICE_FINGERPRINT_SENSOR_TYPE="touch_display_optical"             # Biometric sensor type
+DEVICE_HAVE_LEGACY_FACE_HAL="true"                # Have legacy Samsung 2.0 biometric face libraries
+
+
